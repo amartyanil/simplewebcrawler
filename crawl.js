2cb0e279bdd012f0006fcdce8169aa3155f96401
HPE <hpe@HPEs-MacBook-Air.local>
2023-03-11 19:24:31 +0530

    test added

--- crawl.js
+++ crawl.js
@@ -1,7 +1,10 @@
-import url from 'node:url';
-
 function normalizeURL(urlToBeNormalized)
 {
     urlToBeNormalized.hostname = urlToBeNormalized.hostname.toLower()
     return urlToBeNormalized
+}
+
+module.exports = 
+{
+    normalizeURL
 }
